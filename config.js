0b0135cb5c3aed5bd94ff511af93a5afec091dc0
Kauã Steliano <stelianok@gmail.com>
2020-07-19 09:29:45 -0300

    fixed mistake

--- config.js
+++ config.js
@@ -8,8 +8,11 @@ const connectionString =
     `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
     const pool = new Pool({
+
         connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
         ssl: isProduction,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
     })
 
 module.exports = {pool}
